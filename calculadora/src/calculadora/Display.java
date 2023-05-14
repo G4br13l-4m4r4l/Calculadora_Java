561927ca1e251d7b7028881113b44f1c65de56c3
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-13 23:25:02 -0300

    adding more functions

--- calculadora/src/calculadora/Display.java
+++ calculadora/src/calculadora/Display.java
@@ -1,12 +1,23 @@
 package calculadora;
 
 import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.Font;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Display extends JPanel{
+
+	private final JLabel label;
+	
 	public Display() {
-		setBackground(Color.GREEN);
+		setBackground(new Color(46,49,50));
+		label = new JLabel("123,42");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("courier", Font.PLAIN, 30));
+		setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 25));
+		add(label);
 	}
 }
--- calculadora/src/calculadora/Teclado.java
+++ calculadora/src/calculadora/Teclado.java
@@ -1,12 +1,29 @@
 package calculadora;
 
 import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
 
 import javax.swing.JPanel;
 
 @SuppressWarnings("serial")
 public class Teclado extends JPanel{
+	private final Color COR_CINZA_ESCURO = new Color(68,68,68);
+	private final Color COR_CINZA_CLARO = new Color(99,99,99);
+	private final Color COR_LARANJA = new Color(242,163,60);
+	
 	public Teclado() {
-		setBackground(Color.RED);
+		GridBagLayout layout = new GridBagLayout();
+		GridBagConstraints c = new GridBagConstraints();
+		
+		
+		setLayout(layout);
+		
+	
+	}
+	
+	public void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+		Botao botao = new Botao(texto, cor);
 	}
 }
