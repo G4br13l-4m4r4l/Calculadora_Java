c23e02329a49c32844a88ef59d68c326497fd16f
Gabriel do Amaral <dev.gabriel.amaral@gmail.com>
2023-05-15 22:00:09 -0300

    adding the buttons

--- calculadora/src/calculadora/Teclado.java
+++ calculadora/src/calculadora/Teclado.java
@@ -20,10 +20,39 @@ public class Teclado extends JPanel{
 		
 		setLayout(layout);
 		
+		//primeira linha
+		adicionarBotao("AC", COR_CINZA_ESCURO,c,0,0);
+		adicionarBotao("+/-", COR_CINZA_ESCURO,c,1,0);
+		adicionarBotao("%", COR_CINZA_ESCURO,c,2,0);
+		adicionarBotao("/",COR_LARANJA,c,3,0);
 	
+		//segunda linha
+		adicionarBotao("7", COR_CINZA_CLARO,c,0,1);
+		adicionarBotao("8", COR_CINZA_CLARO,c,1,1);
+		adicionarBotao("9", COR_CINZA_CLARO,c,2,1);
+		adicionarBotao("*",COR_LARANJA,c,3,1);
+		
+		//terceira linha
+		adicionarBotao("4", COR_CINZA_CLARO,c,0,2);
+		adicionarBotao("5", COR_CINZA_CLARO,c,1,2);
+		adicionarBotao("6", COR_CINZA_CLARO,c,2,2);
+		adicionarBotao("-",COR_LARANJA,c,3,2);
+
+		//quarta linha
+		adicionarBotao("1", COR_CINZA_CLARO,c,0,3);
+		adicionarBotao("2", COR_CINZA_CLARO,c,1,3);
+		adicionarBotao("3", COR_CINZA_CLARO,c,2,3);
+		adicionarBotao("+",COR_LARANJA,c,3,3);
+		
+		//quinta linha
+		adicionarBotao("0", COR_CINZA_CLARO,c,0,4);
+		adicionarBotao(".", COR_CINZA_CLARO,c,1,4);
+		adicionarBotao("=",COR_LARANJA,c,3,4);
 	}
-	
 	public void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+		c.gridx =x;
+		c.gridy = y;
 		Botao botao = new Botao(texto, cor);
+		add(botao);
 	}
 }
